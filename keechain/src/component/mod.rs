8967300bac97d491b2f81682d3b696e216b54d9e
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-01-16 14:39:27 +0100

    gui: add `NumericInput`

--- keechain/src/component/mod.rs
+++ keechain/src/component/mod.rs
@@ -3,9 +3,11 @@
 
 pub mod button;
 mod icon;
+mod numeric_input;
 mod text;
 mod text_input;
 
 pub use self::icon::Icon;
+pub use self::numeric_input::NumericInput;
 pub use self::text::Text;
 pub use self::text_input::TextInput;
